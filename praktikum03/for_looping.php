5141c43183d6dcbf18af4ba6661b9658d52350e7
arya0911 <114551899+arya0911@users.noreply.github.com>
2023-03-09 00:07:14 -0800

    Add files via upload

---
+++ praktikum03/for_looping.php
@@ -0,0 +1,10 @@
+<?php
+for($i = 0;$i < 10; $i++){
+    echo "Hello <br>";
+}
+echo "<br>";
+
+$array = ['kucing', 'kuda','beruang'];
+for($i = 0;$i < count($array) ;$i++) {
+    echo $array[$i] . "<br>";
+}
